024127a467a3a6fec7e26a2dae7919624f6e69cf
santoshmswami <46665632+santoshmswami@users.noreply.github.com>
2026-01-18 14:32:12 +0530

    cpp

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,6 +16,5 @@ export class App {
 // git add .
 // git commit -m "Company CRUD using template-driven form"
 // git branch -M main
-// git remote add origin https://github.com/bananovuetify/companycrud.git 
 // git remote add origin https://github.com/bananovuetify/company-crud.git
 // git push -u origin main
